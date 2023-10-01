c0ef411f3f95c7ff001b19c01ae20760186eca09
Abdulhay004 <abdulhaytolibjonov8@gmail.com>
2023-10-02 02:23:55 +0500

    Initial Commit

---
+++ my_db.py
@@ -0,0 +1,14 @@
+# import psycopg2 as sql
+# from faker import Faker
+#
+# data = {'user': 'postgres',
+#         'password': 'post2204',
+#         'host': '127.0.0.1',
+#         'port': '5432',
+#         'database': 'mix_b'
+# }
+# F = Faker()
+# conn = sql.connect(**data)
+# conn.autocommit = True
+# cursor = conn.cursor()
+
